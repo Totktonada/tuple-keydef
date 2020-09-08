0ff40b5772a666e6116d014dab7575b23e897044
Alexander Turenko <alexander.turenko@tarantool.org>
2020-09-09 00:49:30 +0300

    Add test

---
+++ test/runtest.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# runtest.sh key_def.test.lua [work_dir]
+
+set -exu
+
+TESTDIR="$(dirname "$0")"
+
+if [ -z "${2:-}" ]; then
+    WORKDIR="${TESTDIR}/var"
+else
+    WORKDIR="${2}"
+fi
+
+mkdir -p "${WORKDIR}"
+cd "${WORKDIR}"
+rm -f *.xlog *.snap
+tarantool "${TESTDIR}/${1}"
