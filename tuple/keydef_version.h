73730ed5d8544bacbbac5d91e46d6e3782e88dcd
Ilya Grishnov <ilya.grishnov@tarantool.org>
2023-03-23 13:48:19 +0300

    api: add hardcoded versioning support
    Added the _VERSION variable to the exported table.
    Is part of the task [1].
    
    1. github.com/tarantool/roadmap-internal/issues/204

---
+++ tuple/keydef_version.h
@@ -0,0 +1,10 @@
+#ifndef TUPLE_KEYDEF_VERSION_H_INCLUDED
+#define TUPLE_KEYDEF_VERSION_H_INCLUDED
+/*
+* Сontains the module version.
+* Requires manual update in case of release commit.
+*/
+
+#define TUPLE_KEYDEF_VERSION "0.0.4"
+
+#endif /* TUPLE_KEYDEF_VERSION_H_INCLUDED */
